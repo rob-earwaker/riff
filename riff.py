bf1aead2b642d6a697e3c7cc8b19c107c4b29ce1
Rob Earwaker <rob-earwaker@users.noreply.github.com>
2017-09-12 21:30:49 +0100

    Add Chunk abstraction

--- riff.py
+++ riff.py
@@ -1,3 +1,4 @@
+import io
 import struct
 
 
@@ -14,7 +15,7 @@ class ChunkIdInvalid(Exception):
         super().__init__(message)
 
 
-class RiffStream:
+class Stream:
     FOUR_CC_STRUCT = struct.Struct('4s')
     UINT_STRUCT = struct.Struct('<I')
 
@@ -23,7 +24,11 @@ class RiffStream:
 
     @classmethod
     def from_stream(cls, stream):
-        return stream if isinstance(stream, RiffStream) else cls(stream)
+        return stream if isinstance(stream, Stream) else cls(stream)
+
+    @classmethod
+    def from_bytes(cls, bytes):
+        return cls.from_stream(io.BytesIO(bytes))
 
     def read(self, size):
         bytes = self._stream.read(size)
@@ -34,11 +39,38 @@ class RiffStream:
 
     def read_fourcc(self):
         bytes = self.read(4)
-        return RiffStream.FOUR_CC_STRUCT.unpack(bytes)[0].decode('ascii')
+        return Stream.FOUR_CC_STRUCT.unpack(bytes)[0].decode('ascii')
 
     def read_uint(self):
         bytes = self.read(4)
-        return RiffStream.UINT_STRUCT.unpack(bytes)[0]
+        return Stream.UINT_STRUCT.unpack(bytes)[0]
+
+
+class Chunk:
+    def __init__(self, id, size, data):
+        self._id = id
+        self._size = size
+        self._data = data
+
+    @classmethod
+    def from_stream(cls, stream):
+        stream = Stream.from_stream(stream)
+        id = stream.read_fourcc()
+        size = stream.read_uint()
+        data = stream.read(size)
+        return cls(id, size, data)
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def size(self):
+        return self._size
+
+    @property
+    def data(self):
+        return self._data
 
 
 class RiffChunk:
@@ -51,14 +83,13 @@ class RiffChunk:
 
     @classmethod
     def from_stream(cls, stream):
-        stream = RiffStream.from_stream(stream)
-        id = stream.read_fourcc()
-        if not id == RiffChunk.ID:
-            raise ChunkIdInvalid(actual=id, expected=RiffChunk.ID)
-        size = stream.read_uint()
-        format = stream.read_fourcc()
-        data = stream.read(size - RiffStream.FOUR_CC_STRUCT.size)
-        return cls(size, format, data)
+        chunk = Chunk.from_stream(stream)
+        if not chunk.id == RiffChunk.ID:
+            raise ChunkIdInvalid(actual=chunk.id, expected=RiffChunk.ID)
+        data_stream = Stream.from_bytes(chunk.data)
+        format = data_stream.read_fourcc()
+        data = data_stream.read(chunk.size - Stream.FOUR_CC_STRUCT.size)
+        return cls(chunk.size, format, data)
 
     @property
     def id(self):
