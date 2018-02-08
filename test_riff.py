4f3fb2968af3ea22d261afde558a6a6af3d4ac1d
Rob Earwaker <rob-earwaker@users.noreply.github.com>
2018-02-08 21:08:30 +0000

    Add chunk data property tests

--- test_riff.py
+++ test_riff.py
@@ -110,5 +110,17 @@ class Test_Chunk_consumed(TestCase):
         self.assertTrue(chunk.consumed)
 
 
+class Test_Chunk_data(TestCase):
+    def test_is_ChunkData_instance_after_creating_chunk(self):
+        datastream = io.BytesIO(b'MockData')
+        chunk = riff.Chunk.create('MOCK', 8, datastream)
+        self.assertIsInstance(chunk.data, riff.ChunkData)
+
+    def test_is_ChunkData_instance_after_reading_chunk_from_stream(self):
+        stream = io.BytesIO(b'MOCK\x08\x00\x00\x00MockData')
+        chunk = riff.Chunk.readfrom(stream)
+        self.assertIsInstance(chunk.data, riff.ChunkData)
+
+
 if __name__ == '__main__':
     unittest.main()
